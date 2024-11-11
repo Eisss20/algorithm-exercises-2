b7c97bd07713600e3fd43eacd08e5ba697580346
Eisss20 <pajenan.j@gmail.com>
2024-11-11 12:20:15 +0700

    Feat : Answer the exercises

--- ex-2.js
+++ ex-2.js
@@ -1,10 +1,28 @@
 function countPositivesSumNegatives(input) {
-  //Start coding here
+  if (!Array.isArray(input) || input.length === 0) return [];
+
+  let countPositives = 0;
+  let sumOfNegative = 0; 
+
+  for (let number of input ) {
+    if (number > 0 ) {
+      countPositives += 1 ;
+    } else if (number < 0 ) {
+      sumOfNegative += number
+    }
 }
+return [countPositives,sumOfNegative,]
+
+}
+
+/// เก็บค่า จำนวนผลบวก และ เก็บค่าผลรวมของจำนวนลบ
+/// นำมาลูปเพื่อตรวจสอบค่า array ว่ามีทั้งหมดผลบวกกี่จำนวน 
+/// นำมาลูปเพื่อรวมค่า array ที่เป็นผลลบ
 
 let result1 = countPositivesSumNegatives([
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15,
 ]);
+
 console.log(result1); // [10, -65]
 
 let result2 = countPositivesSumNegatives("");
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,13 @@
 function validatePIN(pin) {
-  //Start coding here
+  return (pin.length === 4 || pin.length === 6) && Number(pin).toString() === pin;
 }
+
+
+/// Number(pin) เช็คค่าตั้งแต่แปลงเป็น number สำเร็จ  ถ้าแปลงไม่สำเร็จจะเป็น false ไปเลย
+//// .toString() หลังจากนั้นแปลงเป็น สติงให้เหมือนเดิม 
+//// และนำผลลัพท์มา เทียบ pin
+
+
 
 let result1 = validatePIN("1234");
 console.log(result1); // true
